89e3282e9ba45e76e5ee88c8512d54b6a88a657f
leilong <leilongchn@163.com>
2018-01-04 18:39:58 +0800

    1.4增加新员工培训功能

--- src/cn/xm/exam/utils/DefaultValue.java
+++ src/cn/xm/exam/utils/DefaultValue.java
@@ -14,5 +14,8 @@ public class DefaultValue {
 	public static final Integer YEAR_RANGE = 2;
 	//厂级试卷负责部门字典ID
 	public static final String DICTIONARY_ID = "400001";
-
+	//正式员工入场安全培训ID
+	public static final String REGULAR_EMPLOYEE_TRAIN = "3671a447c95840a89c421e6354edb7e9";
+	//长委新员工培训ID
+	public static final String LONGTERM_EMPLOYEE_TRAIN = "63de874cef6f42cab170648bc84ae8a4";
 }
