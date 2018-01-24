8090343abc4c1369f90d20d4ed59f5816466005b
qiaozhi <qiao_liqiang@163.com>
2018-01-25 00:11:55 +0800

    修改了内部新员工二次分配

--- src/cn/xm/exam/utils/DefaultValue.java
+++ src/cn/xm/exam/utils/DefaultValue.java
@@ -7,16 +7,16 @@ package cn.xm.exam.utils;
  * @time 2017年12月9日下午2:51:19
  */
 public class DefaultValue {
-	public static final String PAGE_SIZE = "8";
-	
 
-	//历史违章记录年限范围，前两年
+	public static final String PAGE_SIZE = "8";
+
+	// 历史违章记录年限范围，前两年
 	public static final Integer YEAR_RANGE = 2;
-	//厂级试卷负责部门字典ID
+	// 厂级试卷负责部门字典ID
 	public static final String DICTIONARY_ID = "400001";
-	//正式员工入场安全培训ID
-	public static final String REGULAR_EMPLOYEE_TRAIN = "94572a6f9cca4c7388e37f25ff774687";
-	//长委新员工培训ID
-	public static final String LONGTERM_EMPLOYEE_TRAIN = "339bbb96d8e44bec8292232f95fe5d46";
+	// 正式员工入场安全培训ID
+	public static final String REGULAR_EMPLOYEE_TRAIN = "b10d26e434b146d5af902d586f809dd9";
+	// 长委新员工培训ID
+	public static final String LONGTERM_EMPLOYEE_TRAIN = "595925c6a55349acb6cc77b3cd47f7a3";
 
 }
