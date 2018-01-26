a5acd8cbbfee2f6b0fb84c1100cb6b067de4e6dc
qiaozhi <qiao_liqiang@163.com>
2018-01-26 20:46:56 +0800

    改了点小东西

--- WebContent/js/newPeopleTrain/newdepartEmpManage.js
+++ WebContent/js/newPeopleTrain/newdepartEmpManage.js
@@ -97,10 +97,10 @@ function showEmployeeBaseInfo(data) {
 				+ "</td><td>"
 				+ "<a href='javascript:void(0)' onclick='allInfo(this)'>详细信息</a>&nbsp;";*/
 		//将培训情况字段内容注释掉
-		showEmployeeOutBaseInfoList +="<a href='javascript:void(0)' onclick='allInfo(this)'>详细信息</a>&nbsp;";
+		showEmployeeOutBaseInfoList +="<a href='javascript:void(0)' title='查看详情' onclick='allInfo(this)'><span class='glyphicon glyphicon-search'></span></a>&nbsp;";
 		if (employeeOutBaseInfoList[i].bigstatus != "已结束" && hasOperatingEmpout) {
-			showEmployeeOutBaseInfoList += "<a href='javascript:void(0)' onclick='el_modifyEmp(this)'>修改</a>&nbsp;"
-					+ "<a class='el_delButton' onClick='delcfm(this)'>删除</a><br />"
+			showEmployeeOutBaseInfoList += "<a href='javascript:void(0)' title='修改员工信息' onclick='el_modifyEmp(this)'><span class='glyphicon glyphicon-pencil'></span></a>&nbsp;"
+					+ "<a class='el_delButton' title='删除员工' onClick='delcfm(this)'><span class='glyphicon glyphicon-trash'></span></a><br />"
 					+ "</td></tr>";
 		} else {
 			showEmployeeOutBaseInfoList += "</td></tr>";
--- WebContent/view/testPerson/newEmployeeAllot.jsp
+++ WebContent/view/testPerson/newEmployeeAllot.jsp
@@ -10,7 +10,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>新员工分配管理</title>
+<title>内部员工培训</title>
 
 <%@ include file="/public/cssJs.jsp"%>
 <link rel="stylesheet" href="<%=path%>/css/outDepart/outdepartTree.css">
@@ -52,7 +52,7 @@ hasOperatingEmpout = true;
 				<div class="panel panel-default">
 					<!--菜单连接标题-->
 					<div class="panel-heading">
-						<span>新员工入厂培训</span><span>>新员工分配管理</span>
+						<span>培训管理</span><span>>员工培训</span><span>>内部员工培训</span>
 					</div>
 					<div class="el_main">
 
