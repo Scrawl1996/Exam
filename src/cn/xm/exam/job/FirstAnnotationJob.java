e78543669498747216a3f3b4062707cb8642b8e6
qiao_zhi <qiao_liqiang@163.com>
2018-11-28 19:08:51 +0800

    代码优化

--- src/cn/xm/exam/job/FirstAnnotationJob.java
+++ src/cn/xm/exam/job/FirstAnnotationJob.java
@@ -2,7 +2,6 @@ package cn.xm.exam.job;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
